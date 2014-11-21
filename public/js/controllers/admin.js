10ffd93309413b4515812e6ed6b8f067a44294f7
afei <afei@55yali.com>
2014-11-21 15:51:15 +0800

    解决datatable插件问题bower install

--- public/js/controllers/admin.js
+++ public/js/controllers/admin.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var adminApp = angular.module('admin', ['ngRoute']);
+var adminApp = angular.module('admin', ['ngRoute','datatables']);
 
 adminApp.config(['$routeProvider', '$locationProvider',
 function($routeProvider, $locationProvider) {
@@ -283,82 +283,82 @@ var colorGenerator = function(){
 }
 
 
-adminApp.directive('datatable', ['$timeout', '$compile',
-  function($timeout, $compile) {
+// adminApp.directive('datatable', ['$timeout', '$compile',
+//   function($timeout, $compile) {
 
-    // default options to be used on to all datatables
-    var defaults = {};
+//     // default options to be used on to all datatables
+//     var defaults = {};
 
-    return {
-      restrict: 'A',
-      compile: function(element, attrs) {
-        var repeatOption = element.find('tr[ng-repeat], tr[data-ng-repeat]'),
-          repeatAttr,
-          watch,
-          original = $(repeatOption).clone();
+//     return {
+//       restrict: 'A',
+//       compile: function(element, attrs) {
+//         var repeatOption = element.find('tr[ng-repeat], tr[data-ng-repeat]'),
+//           repeatAttr,
+//           watch,
+//           original = $(repeatOption).clone();
 
-        // enable watching of the dataset if in use
-        repeatOption = element.find('tr[ng-repeat], tr[data-ng-repeat]');
+//         // enable watching of the dataset if in use
+//         repeatOption = element.find('tr[ng-repeat], tr[data-ng-repeat]');
 
-        if (repeatOption.length) {
-          repeatAttr = repeatOption.attr('ng-repeat') || repeatOption.attr('data-ng-repeat');
-          watch = $.trim(repeatAttr.split('|')[0]).split(' ').pop();
-        }
+//         if (repeatOption.length) {
+//           repeatAttr = repeatOption.attr('ng-repeat') || repeatOption.attr('data-ng-repeat');
+//           watch = $.trim(repeatAttr.split('|')[0]).split(' ').pop();
+//         }
 
-        // post-linking function
-        return function(scope, element, attrs, controller) {
+//         // post-linking function
+//         return function(scope, element, attrs, controller) {
 
-          // merge default options with table specific override options
-          var options = angular.extend({}, defaults, scope.$eval(attrs.datatable)),
-              table = null;
+//           // merge default options with table specific override options
+//           var options = angular.extend({}, defaults, scope.$eval(attrs.datatable)),
+//               table = null;
 
-          // add default css style
-          element.addClass('table table-striped');
+//           // add default css style
+//           element.addClass('table table-striped');
 
-          if (watch) {
+//           if (watch) {
 
-            // deep watching of dataset to re-init on change
-            scope.$watch(watch, function(newValue, oldValue) {
-              if (newValue) {
+//             // deep watching of dataset to re-init on change
+//             scope.$watch(watch, function(newValue, oldValue) {
+//               if (newValue) {
 
-                // check for class, 'fnIsDataTable' doesn't work here
-                if (!element.hasClass('dataTable')) {
+//                 // check for class, 'fnIsDataTable' doesn't work here
+//                 if (!element.hasClass('dataTable')) {
 
-                  // init datatables after data load for first time
-                  $timeout(function() {
-                      table = element.dataTable(options);
-                  });
+//                   // init datatables after data load for first time
+//                   $timeout(function() {
+//                       table = element.dataTable(options);
+//                   });
 
-                } else if (newValue != oldValue) {
+//                 } else if (newValue != oldValue) {
 
-                  // destroy and re-init datatable with new data (fnDraw not working here)
-                  // table.fnDestroy();
+//                   //destroy and re-init datatable with new data (fnDraw not working here)
+//                   // table.fnDestroy();
 
-                  // DataTables addes specifc 'width' property after destroy, have to manually remove
-                  // element.removeAttr('style');
+//                   // //DataTables addes specifc 'width' property after destroy, have to manually remove
+//                   // element.removeAttr('style');
 
-                  // empty the <tbody> to remove old ng-repeat rows and re-compile with new dataset
-                  // var body = element.find('tbody');
-                  // body.empty();
-                  // body.append($compile(original)(scope));
+//                   // //empty the <tbody> to remove old ng-repeat rows and re-compile with new dataset
+//                   // var body = element.find('tbody');
+//                   // body.empty();
+//                   // body.append($compile(original)(scope));
 
-                  // 'timeout' to allow ng-repeat time to render
-                  // $timeout(function() {
-                  //     table.dataTable(options);
-                  // });
+//                   // //'timeout' to allow ng-repeat time to render
+//                   $timeout(function() {
+//                       table.dataTable(options);
+//                   });
 
-                }
-              }
-            }, true);
-          } else {
-            // no dataset present, init normally
-            table = element.dataTable(options);
-          }
-        };
-      }
-    };
-  }
-]);
+//                 }
+//               }
+//             }, true);
+//           } else {
+//             // no dataset present, init normally
+//             table = element.dataTable(options);
+//           }
+//         };
+//       }
+//     };
+//   }
+// ]);
 
 
 
@@ -1790,6 +1790,7 @@ adminApp.controller('ReportController', ['$http','$scope',
                           ];
     $scope.select_status = $scope.report_statuses[0].name;
     $scope.select_type = $scope.report_types[0];
+
     $scope.getReport = function(select_status){
       if(!$scope.select_type.name||!$scope.select_status){
         return;
