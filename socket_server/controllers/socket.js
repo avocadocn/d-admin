180b248e2b4018f94d4f36c21f3560da416a4c2c
Yali-07 <maggie03230@gmail.com>
2014-12-02 19:16:12 +0800

    增加进入room的功能

--- socket_server/controllers/socket.js
+++ socket_server/controllers/socket.js
@@ -8,16 +8,50 @@ module.exports = function (io) {
     console.log('someone connected');
     socket.on('login',function(){
       users[index]=socket.id;
-      console.log('user'+index+'login');
+      var text = 'user'+index+'has logined';
+      console.log(text);
       index++;
+      socket.broadcast.send('message',text);
+      socket.emit('message',text);
+      console.log(io.sockets.adapter.rooms);
     });
+
     socket.on('disconnect',function(){
       // 从users里删掉这个人
       var whereIsUser = _.indexOf(users, socket.id);
       if(whereIsUser!==-1)
         users[whereIsUser] = null;
-      console.log('user'+whereIsUser+'disconnected');
+      var text = 'user'+whereIsUser+'disconnected';
+      console.log(text);
+      socket.broadcast.send(text);
     });
     
+    socket.on('enterRoom',function(roomIndex){
+      socket.join(roomIndex);
+      var whereIsUser = _.indexOf(users, socket.id);
+      var text = 'user'+whereIsUser + 'has entered room'+ roomIndex;
+      console.log(text);
+      // socket.broadcast.to(roomIndex).emit('message',text);
+      io.sockets.in(roomIndex).emit('message', text);
+    });
+
+    socket.on('quitRoom',function(roomIndex){
+      var whereIsUser = _.indexOf(users, socket.id);
+      var text = 'user'+whereIsUser + 'has quitted room'+ roomIndex;
+      console.log(text);
+      socket.leave(roomIndex);
+      // socket.broadcast.to(socket.rooms).emit('message',text);
+      io.sockets.in(roomIndex).emit('message', text);
+    });
+
+    socket.on('talk',function(text){
+      var whereIsUser = _.indexOf(users, socket.id);
+      if(whereIsUser!==-1)
+        users[whereIsUser] = null;
+      var text = 'user'+whereIsUser+':'+text;
+      console.log(socket.rooms[1]);
+      // socket.broadcast.to(socket.rooms).emit('message',text);
+      io.sockets.in(socket.rooms[1]).emit('message', text);
+    });
   });
 };
